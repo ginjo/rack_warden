ac997c35adf072faa4beaaa6cca65473f07a9b6b
wbr <wbr@mac.com>
2015-01-18 18:56:39 -0800

    Changed User#password column name to encrypted_password. Change database column_names with http://sqlitebrowser.org/, since sqlite doesn't do column_name changes on its own.

--- lib/rack_warden/models/user.rb
+++ lib/rack_warden/models/user.rb
@@ -13,31 +13,39 @@ module RackWarden
     property :id, Serial, :key => true
     property :username, String, :length => 128, :unique => true, :required => true, :default => lambda {|r,v| r.instance_variable_get :@email}
     property :email, String, :length => 128, :unique => true, :required => true, :format=>:email_address
-    property :password, BCryptHash
+    property :encrypted_password, BCryptHash, :writer => :protected, :default => lambda {|r,v| r.instance_variable_get :@password}
     
-    attr_accessor :password_confirmation, :'password_required'
+    attr_accessor :password, :password_confirmation
     
     
     ###  VALIDATION  ###
     
-		validates_confirmation_of :password, :if => :password
-		validates_with_method			:password_confirmation, :method => :valid_password_elements, :if => :password
-		validates_length_of				:password_confirmation, :within => 8..40
-		
+    validates_presence_of 		:password, :password_confirmation, :if => :password_required?
+		validates_confirmation_of :password, :if => :password_required?
+		validates_length_of				:password, :min => 8, :if => :password
+		validates_with_method			:password, :method => :valid_password_elements, :if => :password
+		validates_length_of				:password_confirmation, :within => 8..40, :when => [:require_password, :user]
+				
+				
+		# check validity of password if we have a new resource, or there is a plaintext password provided
+    def password_required?
+      password || new?
+    end
+				
 	  # Validation returns nil if valid
 		def valid_password_elements
 			unless password_element_count >= 2
-				_message = "Passwords must be minimum 8 characters in length
+				message = "Passwords must be minimum 8 characters in length
 				and contain at least two of the following character types: uppercase,
 				lowercase, numbers, symbols."
-				[false, _message]
+				[false, message]
 			else
 				true
 			end
 		end
 	
 		# Returns number of specified character classes found in pwd
-		def password_element_count(pwd=password_confirmation, character_classes = %w[upper lower digit punct])
+		def password_element_count(pwd=password, character_classes = %w[upper lower digit punct])
 			character_classes.find_all{|c| pwd.to_s[/[[:#{c}:]]/]}.size
 		rescue
 			0
@@ -47,7 +55,7 @@ module RackWarden
 		###  INSTANCE  ###
 
     def authenticate(attempted_password)
-      if self.password == attempted_password
+      if self.encrypted_password == attempted_password
         true
       else
         false
