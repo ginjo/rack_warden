e81c54daa6914ed7c0984af51fb909363716190b
wbr <wbr@mac.com>
2015-01-23 04:59:15 -0800

    Minor changes to log output.

--- lib/rack_warden/routes.rb
+++ lib/rack_warden/routes.rb
@@ -80,7 +80,7 @@ module RackWarden
 						#redirect "/auth/login"
 						return_to (logged_in? ? '/auth' : '/auth/login')
 					else
-						App.logger.info "RW /auth/activate failed for '#{@user.username rescue nil}' #{$!}"
+						App.logger.info "RW /auth/activate failed for '#{@user}' with errors: #{$!}"
 						halt "Could not activate", :layout=>settings.layout
 					end
 				end
