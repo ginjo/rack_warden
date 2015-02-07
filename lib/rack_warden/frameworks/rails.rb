e4c7dd9d6ff8c04acdc55bb00aa36185de34b00e
wbr <wbr@mac.com>
2015-02-06 23:48:31 -0800

    Eliminated generic interim module when registering protected helpers into rails framework.

--- lib/rack_warden/frameworks/rails.rb
+++ lib/rack_warden/frameworks/rails.rb
@@ -5,7 +5,7 @@ module RackWarden
       extend Frameworks
 
       def selector
-        App.logger.debug "RW Rails.selector parent_app.ancestors #{parent_app.ancestors}"
+        App.logger.debug "RW Frameworks::Rails.selector" # parent_app.ancestors #{parent_app.ancestors}
         parent_app.ancestors.find{|x| x.to_s[/Rails|ActionDispatch/]} or defined?(::Rails)
       end
       
@@ -16,7 +16,7 @@ module RackWarden
 
       module ClassMethods
 				def require_login(*args)
-					App.logger.debug "RW running #{self}.require_login(#{args.inspect})"
+					App.logger.debug "RW Frameworks::Rails::ClassMethods #{self}.require_login(#{args.inspect})"
 					#before_filter(*[:require_login, args].flatten.compact)
 					before_filter(*args) do |controller|
 						controller.send :require_login
@@ -26,16 +26,15 @@ module RackWarden
 
 
       def setup_framework
-        App.logger.debug "RW setup_framework for rails"
-    		m = Module.new.include(RackWarden::UniversalHelpers)
-    		m.send :protected, *(m.instance_methods)
-    		ActionController::Base.send(:include, m)
+        App.logger.debug "RW Frameworks::Rails.setup_framework"
+    		ActionController::Base.send(:include, RackWarden::UniversalHelpers)
+    		ActionController::Base.send(:protected, *RackWarden::UniversalHelpers.instance_methods)
 
     		ActionController::Base.helper_method UniversalHelpers.instance_methods
     			      
 	      # Define class method 'require_login' on framework controller.
 	      # Note that rails before-filters are also class methods, thus the need to differentiate method names (is this correct?).
-	      App.logger.info "RW defining ActionController::Base.require_login"
+	      App.logger.info "RW Frameworks::Rails.setup_framework ActionController::Base.extend ClassMethods"
 				# ActionController::Base.define_singleton_method :require_login do |*args|
 				# 	conditions_hash = args[0] || Hash.new
 				# 	before_filter(:require_login, conditions_hash)
